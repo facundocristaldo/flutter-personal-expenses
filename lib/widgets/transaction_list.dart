23434c11f3ac4bda0fad2b0b8bf66e6f9c43d731
Facundo Cristaldo <facu.cristaldo.96@gmail.com>
2021-07-07 22:29:37 -0300

    refactoring

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'transaction_item.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
@@ -32,42 +32,10 @@ class TransactionList extends StatelessWidget {
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
-              return Card(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                elevation: 5,
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: EdgeInsets.all(6),
-                      child: FittedBox(
-                          child: Text('\$${transactions[index].amount}')),
-                    ),
-                  ),
-                  title: Text(
-                    transactions[index].title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Text(
-                    DateFormat.yMMMd().format(transactions[index].date),
-                  ),
-                  trailing: mediaQuery.size.width > 460
-                      ? FlatButton.icon(
-                          icon: Icon(Icons.delete),
-                          label: Text("Delete"),
-                          textColor: Theme.of(context).errorColor,
-                          onPressed: () {
-                            deleteTransaction(transactions[index].id);
-                          },
-                        )
-                      : IconButton(
-                          icon: Icon(Icons.delete),
-                          color: Theme.of(context).errorColor,
-                          onPressed: () {
-                            deleteTransaction(transactions[index].id);
-                          },
-                        ),
-                ),
+              return TransactionItem(
+                transaction: transactions[index],
+                mediaQuery: mediaQuery,
+                deleteTransaction: deleteTransaction,
               );
             },
           );
